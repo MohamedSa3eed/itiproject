09161075327dd17e4289e7c770303b2623bc9d04
Hanan Saeed <89020289+HananSaeed@users.noreply.github.com>
2022-09-10 18:49:30 +0200

    Update func.c

--- APP/func.c
+++ APP/func.c
@@ -1,38 +1,16 @@
+#include "interface.h"
 #include "config.h"
 #include "../MCAL/DIO/DIO_INTERFACE.h"
 #include "../MCAL/EXT/EXT_INTERFACE.h"
 #include "../MCAL/TIMER/TIMER_INTERFACE.h"
 #include "../MCAL/GI/GI_INTERFACE.h"
 #include "../HAL/LCD/LCD_INTERFACE.h"
+#include "../HAL/KEYPAD/KPD_interface.h"
+#include "../HAL/KEYPAD/KPD_config.h"
 #include "../HAL/KEYPAD/KPD_PRIVATE.h"
 #include "../MCAL/ADC/ADC_INTERFACE.h"
 #include "../HAL/LCD/LCD_CONFIG.h"
 #include "avr/delay.h"
-
-#include "../HAL/KEYPAD/KPD_CONFIG.h"
-#include "../HAL/KEYPAD/KPD_INTERFACE.h"
-void openDoor(void);
-void openFan(void);
-u8 getOption(void);
-u32 getPass(void);
-u8 Login(void);
-void loginInit(void);
-void roomOptions(void);
-u8 checkUser1Pass(void);
-u8 checkUser2Pass(void);
-u8 checkUser3Pass(void);
-void room1On(void);
-void room1Off(void);
-void room2On(void);
-void room2Off(void);
-void room3On(void);
-void room3Off(void);
-void options(void);
-void WrongPass(void);
-void rigthPass(void);
-void wrongOption(void);
-void closeFan(void);
-void ldrCheck(void);
 
 u32 savedpass1 = 1111;
 u32 savedpass2 = 2222;
@@ -83,13 +61,13 @@ void openFan(void)
 }
 void closeFan(void)
 {
-	PWM_CTC_VALUE(0);
+
 }
 
 u8 Login(void)
 {
 	loginInit();
-	ldrCheck();
+
 	u8 user = getOption();  //get user from keypad
 	LCD_WriteCommand(clear);
 	LCD_GoToXY(0, 0);
@@ -168,7 +146,8 @@ u8 Login(void)
 
 	}
 	default :
-		wrongOption();
+		LCD_WriteString("Wrong Option");
+		_delay_ms(100);
 		return 0 ;
 	}
 
@@ -307,7 +286,6 @@ void options(void)
 			_delay_ms(20);
 			options();
 		}
-		break ;
 	}
 
 	case  Back :
@@ -321,8 +299,8 @@ void options(void)
 		LCD_WriteCommand(clear);
 		options();
 	}
-}
-LCD_GoToXY(0, 0);
+	}
+	LCD_GoToXY(0, 0);
 
 }
 u8 getOption(void)
@@ -429,10 +407,6 @@ void roomOptions(void)
 		}
 		break;
 	}
-	default :
-	{
-		wrongOption();
-	}
 	}
 	LCD_WriteCommand(clear);
 	options();
@@ -454,29 +428,94 @@ void WrongPass(void)
 	LCD_WriteCommand(clear);
 
 }
-void ldrCheck()
+
+void ldrCheck(void)
 {
+	u16 Local_AnaloglValue,Local_DigitalValue;
+
+	DIO_SetPinDirection(PORTB,3,DIO_OUTPUT);
+	DIO_SetPinDirection(PORTA,1,DIO_INPUT);
+	MADC_voidInit();
 	u16 reading = MADC_u8StartConversion(ADC_CHANNEL_1);
-		u16 volt_reading =((reading*5000UL)/1024);
-		if(volt_reading < 3000 )
-		{
-			room1On();
-			room2On();
-			room3On();
+	u16 volt_reading =((reading*5000UL)/1024);
 
-		}
-		else
-		{
-			room1Off();
-			room2Off();
-			room3Off();
+	if(volt_reading > 3500)
+	{
+		room1On();
+		room2On();
+		room3On();
+	}
 
-		}
+	else
+	{
+		room1Off();
+		room2Off();
+		room3Off();
+	}
+
+
+
+
 }
-void wrongOption(void)
+
+void ControlFan(void)
 {
-	LCD_WriteString("Wrong Option");
-			_delay_ms(100);
-	LCD_WriteCommand(clear);
+	u16 Local_AnaloglValue,Local_DigitalValue;
+	DIO_SetPinDirection(PORTA,0,DIO_INPUT);
+	MADC_voidInit();
+	DIO_SetPinDirection(PORTB,3,DIO_OUTPUT);
+
+
+
+	Local_DigitalValue = MADC_u8StartConversion(0);
+	Local_AnaloglValue = (Local_DigitalValue * 5000UL)/1024;
+
+	if(Local_AnaloglValue>430)
+	{
+		TIMER1_INT();
+		PWM_INIT_FAST();
+		PWM_CTC_VALUE((Local_AnaloglValue/10)*3);
+		_delay_ms(5);
+	}
+	else
+	{
+		DIO_SetPinDirection(PORTB,3,DIO_INPUT);
+		PWM_CTC_VALUE(0);
+	}
+
+
+
 }
 
+void keypadinit(void)
+{
+	// columns pin direction as output
+	DIO_SetPinDirection(KPD_PORT, KPD_COLUMN0_PIN, DIO_OUTPUT);
+	DIO_SetPinDirection(KPD_PORT, KPD_COLUMN1_PIN, DIO_OUTPUT);
+	DIO_SetPinDirection(KPD_PORT, KPD_COLUMN2_PIN, DIO_OUTPUT);
+	DIO_SetPinDirection(KPD_PORT, KPD_COLUMN3_PIN, DIO_OUTPUT);
+
+	// rows pins direction as input
+	DIO_SetPinDirection(KPD_PORT, KPD_ROW1_PIN, DIO_INPUT );
+	DIO_SetPinDirection(KPD_PORT, KPD_ROW1_PIN, DIO_INPUT );
+	DIO_SetPinDirection(KPD_PORT, KPD_ROW2_PIN, DIO_INPUT );
+	DIO_SetPinDirection(KPD_PORT, KPD_ROW3_PIN, DIO_INPUT );
+	// setting all high
+	DIO_SetPortValue(KPD_PORT,0xff);
+
+	//rooms
+	DIO_SetPinDirection(PORTB, DIO_PIN4, DIO_OUTPUT);
+	DIO_SetPinDirection(PORTB, DIO_PIN5, DIO_OUTPUT);
+	DIO_SetPinDirection(PORTB, DIO_PIN6, DIO_OUTPUT);
+	LCD_Init();
+	DIO_SetPinDirection(PORTA,1,DIO_OUTPUT);
+	DIO_SetPinDirection(PORTD,2,DIO_INPUT);
+	DIO_SetPinDirection(PORTA,2,DIO_OUTPUT);
+	DIO_SetPinDirection(PORTD,3,DIO_INPUT);
+	GI_Enable();
+	SetCallBack(&ldrCheck,EXT_INT0);
+	SetCallBack(&ControlFan,EXT_INT1);
+	ExtEnable(EXT_INT0,EXT_ANY_LOGICAL);
+	ExtEnable(EXT_INT1,EXT_ANY_LOGICAL);
+
+}
